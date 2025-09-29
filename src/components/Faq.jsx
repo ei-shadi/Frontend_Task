158418b6c16e84c52ebacddc6a24af4344fb45cb
ei-shadi <eftajulislam.saadi@gmail.com>
2025-09-29 11:38:56 +0600

    File Setup

---
+++ src/components/Faq.jsx
@@ -0,0 +1,16 @@
+
+
+const Faq = () => {
+  return (
+    <section>
+      <h1>This is Faq Section</h1>
+
+      {/* Question Container */}
+      <div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Faq;
---
+++ src/components/GetInTouch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetInTouch = () => {
+  return (
+    <div>
+      This is the Get In Touch Section
+    </div>
+  );
+};
+
+export default GetInTouch;
